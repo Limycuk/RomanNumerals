2c3e5bca32e267cd691b7ff279a3d4c765c2d9f7
Alexander Baltsevich <baltsevicha@gmail.com>
2019-06-15 03:51:04 +0300

    Updates

---
+++ precache-manifest.877de0fdde06d50a3cb5a514c4565356.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7578cd580aa30b0a9d0221fd7abd1ff3",
+    "url": "/RomanNumerals/index.html"
+  },
+  {
+    "revision": "776a8d2791a586c68344",
+    "url": "/RomanNumerals/static/css/main.5e6e0066.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/RomanNumerals/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "776a8d2791a586c68344",
+    "url": "/RomanNumerals/static/js/main.a153a433.chunk.js"
+  },
+  {
+    "revision": "2b8a738c185c04f8ccf3",
+    "url": "/RomanNumerals/static/js/runtime~main.9dadf4e2.js"
+  }
+]);
